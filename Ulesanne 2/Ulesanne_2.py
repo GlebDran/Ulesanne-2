75f5d47237736cdda1696413ebb7935daf2de9e4
Gleb Dranitsyn <Gleb Dranitsyn@GLEB-WORK-PC>
2024-11-26 12:49:31 +0200

    Добавьте файлы проекта.

---
+++ Ulesanne 2/Ulesanne_2.py
@@ -0,0 +1,15 @@
+
+vanus = 18
+eesnimi = "Jaak"
+pikkus = 16.5
+kas_käib_koolis = True
+
+# Типы переменных
+print(type(vanus))          # int
+print(type(eesnimi))        # str
+print(type(pikkus))         # float
+print(type(kas_käib_koolis))  # bool
+
+# Варианты значения для `kas_käib_koolis`
+kas_käib_koolis = False
+print(type(kas_käib_koolis))
